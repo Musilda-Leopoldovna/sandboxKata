15c80a9b5b9c6d8cd2fef7ac64a3bdfc8a84f744
Musilda-Leopoldovna <musya_leo_4024@aleeas.com>
2024-05-07 18:45:28 +0200

    5_KataCommit: prepare

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+
+import java.util.List;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+        userService.saveUser("Kolya", "Ivanov", (byte) 5);
+        userService.saveUser("Misha", "Ivanov", (byte) 6);
+        userService.saveUser("Sasha", "Petrov", (byte) 5);
+        userService.saveUser("Sasha", "Utochkin", (byte) 7);
+        userService.saveUser("Kolya", "Ivanov", (byte) 5);
+
+        List<User> allUsers = userService.getAllUsers();
+        allUsers.forEach(u -> System.out.println(u.toString()));
+
+        userService.removeUserById(3L);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
