1f6eaf9d7015bc7cfd91b8079cc9657fe2a9a160
Musilda-Leopoldovna <musya_leo_4024@aleeas.com>
2024-05-17 23:48:51 +0200

    Correction SessionFactory

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -10,45 +10,48 @@ import java.util.List;
 import java.util.logging.*;
 
 public class UserDaoHibernateImpl implements UserDao {
-    private final Util util = new Util();
+    private final SessionFactory sessionFactory = getSessionFactory();
     private static final Logger LOGGER = Logger.getLogger(UserDaoHibernateImpl.class.getName());
     public UserDaoHibernateImpl() {
     }
-
-    @Override
-    public void createUsersTable() {
-        try (Session session = util.getHibernateConnect().openSession()){
-            Transaction transaction = session.beginTransaction();
-            session.createNativeQuery("CREATE TABLE IF NOT EXISTS user");
-            transaction.commit();
+    private SessionFactory getSessionFactory() {
+        try {
+            return new Util().getHibernateConnect();
         } catch (ClassNotFoundException | IOException e) {
             throw new RuntimeException(e.fillInStackTrace());
+        }
+    }
+    @Override
+    public void createUsersTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS user " + "(ID BIGINT(100) NOT NULL PRIMARY KEY AUTO_INCREMENT, " +
+                "name varchar(20) NOT NULL, lastname VARCHAR(40) NOT NULL, age integer(3) NOT NULL, " +
+                "UNIQUE INDEX ID (name, lastname, age))";
+        try(Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createNativeQuery(sql).executeUpdate();
+            transaction.commit();
         }
     }
 
     @Override
     public void dropUsersTable() {
         String sql = "DROP TABLE IF EXISTS user";
-        try (Session session = util.getHibernateConnect().openSession()){
+        try(Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
             session.createNativeQuery(sql).executeUpdate();
             transaction.commit();
-        } catch (ClassNotFoundException | IOException e) {
-            throw new RuntimeException(e.fillInStackTrace());
         }
     }
     @Override
     public void saveUser(String name, String lastName, byte age) {
         User user = new User(name, lastName, age);
-        try(Session session = util.getHibernateConnect().openSession()) {
+        try(Session session = sessionFactory.openSession()) {
             if (session == null)
                 return;
             Transaction transaction = session.beginTransaction();
             session.save(user);
             LOGGER.log(Level.INFO, "User name '" + name + " " + lastName + "' added in DB.");
             transaction.commit();
-        } catch (ClassNotFoundException | IOException e) {
-            throw new RuntimeException(e.fillInStackTrace());
         } catch (JDBCException e) {
             e.fillInStackTrace();
         }
@@ -57,32 +60,26 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void removeUserById(long id) {
         String hql = "DELETE User where ID = :param";
-        try(Session session = util.getHibernateConnect().openSession()) {
+        try(Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
             session.createQuery(hql).setParameter("param", id).executeUpdate();
             transaction.commit();
-        } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e.fillInStackTrace());
         }
     }
 
     @Override
     public List<User> getAllUsers() {
-        try(Session session = util.getHibernateConnect().openSession()){
+        try(Session session = sessionFactory.openSession()){
             return session.createQuery("FROM User", User.class).getResultList();
-        } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e.fillInStackTrace());
         }
     }
 
     @Override
     public void cleanUsersTable() {
-        try(Session session = util.getHibernateConnect().openSession()) {
+        try(Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
             session.createQuery("DELETE FROM User").executeUpdate();
             transaction.commit();
-        } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e.fillInStackTrace());
         }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,12 +1,15 @@
 package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
-import org.hibernate.*;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.io.*;
-import java.nio.file.*;
-import java.sql.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
